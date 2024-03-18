718ba2c8ace75e78b9c225c070a981d294a57a1d
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-03-18 15:46:50 -0500

    fetch data, map and return data, handle pagination with onclick events

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [products, setProducts] = useState([])
+  const [page, setPage] = useState(1)
+
+  const fetchProducts = async () => {
+     const res = await fetch("https://dummyjson.com/products?limit=100")
+     const data = await res.json()
+
+     if (data && data.products) {
+       setProducts(data.products)
+      }
+  }
+
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
+  const selectPageHandler = (selectedPage) => {
+    if(
+      selectedPage >= 1 &&
+      selectedPage <= products.length / 10 &&
+      selectedPage !== page
+    )
+    setPage(selectedPage)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='app'>
+      {
+        products.length > 0 && (
+          <div className='products'>
+            {
+              products.slice(page * 10 - 10, page * 10).map((product) => {
+                return (
+                  <span className='products__single' key={product.id}>
+                    <img src={product.thumbnail} alt={product.title} />
+                    <span>{product.title}</span>
+                  </span>
+                )
+              })
+            }
+          </div>
+      )}
+      {
+        products.length > 0 && (
+          <div className='pagination'>
+            <span 
+              onClick={() => selectPageHandler(page - 1)}
+              className={page > 1 ? "" : "pagination__disable"}
+            >
+              ⬅️
+            </span>
+              {
+                [...Array(products.length / 10)].map((_, index) => {
+                  return (
+                  <span 
+                    key={index} 
+                    onClick={() => selectPageHandler(index + 1)}
+                    className={page === index + 1 ? "pagination__selected" : ""}
+                  >
+                    {index + 1}
+                  </span>
+                  )
+                })
+              }
+            <span 
+              onClick={() => selectPageHandler(page + 1)}
+              className={page < products.length / 10 ? "" : "pagination__disable"}
+            >
+              ➡️
+            </span>
+          </div>
+        )
+      }
+    </div>
   )
 }
 
